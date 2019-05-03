a15e8fce705bd985ea64443733fcd1fcdaa2f82f
Aishwarya Subramanian <ashdankk@Ashs-MacBook-Pro.local>
2019-05-02 20:09:25 -0700

    new problem

---
+++ oddeven.py
@@ -0,0 +1,28 @@
+'''Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. Hint: how does an even / odd number react differently when divided by 2?
+
+Extras:
+
+If the number is a multiple of 4, print out a different message.
+Ask the user for two numbers: one number to check (call it num) and one number to divide by (check). If check divides evenly into num, tell that to the user. If not, print a different appropriate message.'''
+
+x = int(input("Enter a number: "))
+
+if x % 2 == 0:
+    print("Even!")
+else:
+    print("Odd!")
+
+b = int(input("Enter a number: "))
+
+if b % 4 == 0:
+    print ("Div by 4")
+else:
+    print ("Not div by 4")
+
+num = int(input("Enter a number: "))
+check = int(input("Enter a number: "))
+
+if num % check == 0:
+    print ("yes")
+else:
+    print ("no")
