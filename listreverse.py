f2f255a69002e75d7a0f0f371a61086b89239ce3
Aishwarya Subramanian <ashdankk@Ashs-MacBook-Pro.local>
2019-05-09 17:18:06 -0700

    palindrome test

---
+++ listreverse.py
@@ -0,0 +1,9 @@
+'''Ask the user for a string and print out whether this string is a palindrome or not.
+A palindrome is a string that reads the same forwards and backwards.'''
+
+#string_letters = []
+string = raw_input("Enter a word/phrase: ")
+string_2 = string[::-1]
+
+if string == string_2:
+    print ("This is a palindrome")
