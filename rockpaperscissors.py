6ae75c2ad992558b1cd255389c4e9fd8dddead7b
Aishwarya Subramanian <ashdankk@Ashs-MacBook-Pro.local>
2019-05-09 17:51:26 -0700

    rock paper s game

---
+++ rockpaperscissors.py
@@ -0,0 +1,34 @@
+'''Make a two-player Rock-Paper-Scissors game.
+(Hint: Ask for player plays (using input), compare them,
+print out a message of congratulations to the winner, and ask if the players want to start a new game)
+
+Remember the rules:
+
+Rock beats scissors
+Scissors beats paper
+Paper beats rock'''
+
+def game(a,b):
+    if a == b:
+        print "draw"
+    else:
+        if a == "R" and b == "S":
+            print "player 1 won"
+        elif b == "R" and a == "S":
+            print "player 2 won"
+        elif a == "S" and b == "P":
+            print "Player 1 won"
+        elif b == "S" and a == "P":
+            print "Player 2 won"
+        elif a == "P" and b == "R":
+            print "player 1 won"
+        elif b == "P" and a == "R":
+            print "player 2 won"
+
+x = raw_input("Would you like to start a new game? (Y/N)" )
+if x == "Y":
+    a = raw_input("Player 1: R,P,S? ")
+    b = raw_input("Player 2: R,P,S? ")
+    game(a,b)
+else:
+    print ("Thanks, next time")
