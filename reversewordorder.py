1df31d9d4b37cea40df6129fd4a6cde251a58c9c
Aishwarya Subramanian <ashdankk@Ashs-MacBook-Pro.local>
2019-05-22 16:45:27 -0700

    reversing using split

---
+++ reversewordorder.py
@@ -0,0 +1,14 @@
+'''Write a program (using functions!) that asks the user for a long string containing multiple words.
+Print back to the user the same string, except with the words in backwards order. For example, say I type the string:
+
+  My name is Michele
+Then I would see the string:
+
+  Michele is name My
+shown back to me.'''
+
+string = raw_input("Enter a sentence: ")
+a = string.split(" ")
+b = a[::-1]
+c = ' '.join(b)
+print c
