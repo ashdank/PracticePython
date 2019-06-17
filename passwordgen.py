26a73532767af2eb04c0a9d81ba54e2ae78e2d07
Aishwarya Subramanian <ashdankk@Ashs-MacBook-Pro.local>
2019-06-16 18:54:30 -0700

    SELECT FROM LONG STRING

---
+++ passwordgen.py
@@ -0,0 +1,12 @@
+'''Write a password generator in Python.
+Be creative with how you generate passwords - strong passwords have a mix of
+lowercase letters, uppercase letters, numbers, and symbols. The passwords should be random,
+generating a new password every time the user asks for a new password.
+Include your run-time code in a main method.'''
+
+import random
+
+s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
+passlen = 8
+p =  "".join(random.sample(s,passlen))
+print p
