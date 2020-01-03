a7cd9316a0adfd7e27a477e4e665873534cc6c57
Aishwarya Subramanian <asubramanian@Fetchs-MacBook-Pro-4.local>
2020-01-03 15:07:09 -0800

    find the oldest dog using class

--- dog_class.py
+++ dog_class.py
@@ -1,1 +1,20 @@
-s = 0
+class Dog:
+
+    # Class Attribute
+    species = 'mammal'
+
+    # Initializer / Instance Attributes
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+# Instantiate the Dog object
+philo = Dog("Philo", 5)
+mikey = Dog("Mikey", 6)
+blue = Dog("Blue", 10)
+
+def max_ages(*args):
+    return max (*args)
+
+print "The oldest dog is " + str(max_ages(philo.age,mikey.age,blue.age)) + " years old"
