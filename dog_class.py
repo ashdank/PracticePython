78957dffd5b520dcd4dc5bd1daae804c2c4c2a6e
Aishwarya Subramanian <asubramanian@Fetchs-MacBook-Pro-4.local>
2020-01-03 16:12:21 -0800

    updated class inheritance

--- dog_class.py
+++ dog_class.py
@@ -1,5 +1,5 @@
-class Dog:
-
+#program to find the oldest dog
+'''class Dog:
     # Class Attribute
     species = 'mammal'
 
@@ -7,7 +7,6 @@ class Dog:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-
 
 # Instantiate the Dog object
 philo = Dog("Philo", 5)
@@ -18,3 +17,69 @@ def max_ages(*args):
     return max (*args)
 
 print "The oldest dog is " + str(max_ages(philo.age,mikey.age,blue.age)) + " years old"
+'''
+
+# program to show dog inheritance
+'''Sample output:
+I have 3 dogs.
+Tom is 6.
+Fletcher is 7.
+Larry is 9.
+And they're all mammals, of course.'''
+
+class Pets:
+
+    dogs = []
+
+    def __init__(self, dogs):
+        self.dogs = dogs
+
+class Dog:
+
+    # Class attribute
+    species = 'mammal'
+
+    # Initializer / Instance attributes
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    # Instance method
+    def description(self):
+        return self.name, self.age
+
+    # Instance method
+    def speak(self, sound):
+        return "%s says %s" % (self.name, sound)
+
+    # Instance method
+    def eat(self):
+        self.is_hungry = False
+
+
+# Child class (inherits from Dog class)
+class RussellTerrier(Dog):
+    def run(self, speed):
+        return "%s runs %s" % (self.name, speed)
+
+
+# Child class (inherits from Dog class)
+class Labrador(Dog):
+    def run(self, speed):
+        return "%s runs %s" % (self.name, speed)
+
+# Create instances of dogs
+my_dogs = [
+    Labrador("Tom", 6),
+    RussellTerrier("Fletcher", 7),
+    Dog("Larry", 9)
+]
+
+my_pets = Pets(my_dogs)
+
+# Output
+print("I have {} dogs.".format(len(my_pets.dogs)))
+for dog in my_pets.dogs:
+    print("{} is {}.".format(dog.name, dog.age))
+
+print("And they're all {}s, of course.".format(dog.species))
