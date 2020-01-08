bb4e4bd19835426228666dff878c4f8bd18db6af
Aishwarya Subramanian <asubramanian@Fetchs-MacBook-Pro-4.local>
2020-01-07 17:04:51 -0800

    check if dogs are hungry

--- dog_class.py
+++ dog_class.py
@@ -1,10 +1,15 @@
 # program to show dog inheritance
-'''Sample output:
+'''Using the same file, add an instance attribute of is_hungry = True to the Dog class.
+Then add a method called eat() which changes the value of is_hungry to False when called.
+Figure out the best way to feed each dog and then output My dogs are hungry.
+if all are hungry or My dogs are not hungry.
+if all are not hungry. The final output should look like this:
 I have 3 dogs.
 Tom is 6.
 Fletcher is 7.
 Larry is 9.
-And they're all mammals, of course.'''
+And they're all mammals, of course.
+My dogs are not hungry.'''
 
 class Pets:
 
@@ -60,3 +65,13 @@ print("I have {} dogs".format(len(my_dogs)))
 for dog in my_dogs:
     print("{} is {}.".format(dog.name, dog.age))
 print("And they're all {}s, of course.".format(dog.species))
+
+for dog in my_dogs:
+    dog.eat()
+    dog_ate = False
+    if dog.is_hungry:
+        dog_ate = True
+if dog_ate:
+    print "My dogs are hungry"
+else:
+    print "They're not hungry"
