bfe19c635c7a1063462af8551c3e3d3930984dd4
Aishwarya Subramanian <asubramanian@Fetchs-MacBook-Pro-4.local>
2020-01-07 16:53:31 -0800

    class inheritance

--- dog_class.py
+++ dog_class.py
@@ -1,24 +1,3 @@
-#program to find the oldest dog
-'''class Dog:
-    # Class Attribute
-    species = 'mammal'
-
-    # Initializer / Instance Attributes
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-# Instantiate the Dog object
-philo = Dog("Philo", 5)
-mikey = Dog("Mikey", 6)
-blue = Dog("Blue", 10)
-
-def max_ages(*args):
-    return max (*args)
-
-print "The oldest dog is " + str(max_ages(philo.age,mikey.age,blue.age)) + " years old"
-'''
-
 # program to show dog inheritance
 '''Sample output:
 I have 3 dogs.
